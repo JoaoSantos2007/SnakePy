636709319c20ad699ad094911454398d19a92e58
João Pedro Tomaz dos Santos <80163527+JoaoSantos2007@users.noreply.github.com>
2021-05-26 08:08:33 -0300

    Revert "Revert "Merge branch 'main' of https://github.com/JoaoSantos2007/jogoCobrinha into main""
    This reverts commit d35b19a53c159f05047f2c11b48add2447aa835c.

--- Snake.py
+++ Snake.py
@@ -18,10 +18,16 @@ def iniciar(inicio,tela,fonte,texto):
     
 while True:
     status = True
-    pygame.mixer.init()
     pygame.init()
-    pygame.mixer.music.load('supermario.mp3')
-    pygame.mixer.music.play()
+    player = AudioPlayer("supermario.mp3")
+    comer = AudioPlayer("comer.mp3")
+    erro = AudioPlayer("Erro.mp3")
+
+    player.play()
+
+    # pygame.mixer.init()
+    # pygame.mixer.music.load('supermario.mp3')
+    # pygame.mixer.music.play()
 
     # Definir cores
     cor_inicio = (64,193,255)
@@ -142,6 +148,7 @@ while True:
         y_novo = head[1] + dy
 
         if head[0] == x_comida and head[1] == y_comida:
+            comer.play()
             lista_cobra.append([x_novo, y_novo])
             tempo = tempo + 0.5
             x_comida = round(random.randrange(0, 600 -d)/20)*20
@@ -186,6 +193,7 @@ while True:
         status = verifica_mordeu_cobra(lista_cobra,status)
         pontuação = atualizar_pontos(lista_cobra)
         clock.tick(tempo)
+    erro.play()
     pygame.display.update()    
     tela.fill(cor_fim)
     fim = fonte2.render("Gamer Over: ", True, cor_pontos)
